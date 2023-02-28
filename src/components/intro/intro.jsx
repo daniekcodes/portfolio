865585310c944d7b771117e0b06630cfd5d1ce36
Daniek <info@daniek.io>
2023-02-28 14:21:40 +0100

    add

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,29 @@
+import "./intro.css"
+
+const Intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, my name is</h2>
+                    <h1 className="i-name">Daniek Schuiling</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">UI/UX Designer</div>
+                            <div className="i-title-item">Photographer</div>
+                            <div className="i-title-item">Writer</div>
+                            <div className="i-title-item">Content Creator</div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+            <div className="i-right">right</div>
+        </div>
+    )
+}
+
+export default Intro
