bf726470b57ab622067f82f37aa626bf3604b1aa
Daniek Schuiling <info@daniek.io>
2023-03-01 13:32:08 +0100

    add

--- src/components/productList/productlist.jsx
+++ src/components/productList/productlist.jsx
@@ -1,24 +1,30 @@
 import "./productlist.css";
 import Product from "../product/product";
+import {products} from "../../data";
 
 const ProductList = () => {
     return (
         <div className="pl">
             <div className="pl-texts">
-                <h1 className="pl-title">Create & inspire. It's Lama</h1>
+                <h1 className="pl-title">Create & inspire.</h1>
                 <p className="pl-desc">
-                    Lama is a creative portfolio that your work has been waiting for.
-                                                  Beautiful homes, stunning portfolio styles & a whole lot more awaits
-                                                  inside.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate obcaecati odio laudantium dolores exercitationem reprehenderit hic aut temporibus iure similique et, pariatur perferendis illum at rem! Voluptatem quia cum odit?
                 </p>
             </div>
             <div className="pl-list">
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-            </div>
+                {
+                products.map((item) => (
+                    <Product key={
+                            item.id
+                        }
+                        img={
+                            item.img
+                        }
+                        link={
+                            item.link
+                        }/>
+                ))
+            } </div>
         </div>
     );
 }
