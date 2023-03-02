52bfdcb837531ccba1b4e0afec8335ef04cb88cc
Daniek <info@daniek.io>
2023-03-02 09:44:32 +0100

    add

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -4,24 +4,23 @@ const About = () => {
     return (
         <div className="a">
             <div className="a-left">
-                <div className="a-card bg"></div>
+                {/* <div className="a-card bg"></div> */}
                 <div className="a-card">
                     <img src={meabout}
                         alt=""
-                        className="a-img"/>
+                        className="a-img" />
                 </div>
             </div>
             <div className="a-right">
                 <h1 className="a-title">About Me</h1>
                 <p className="a-sub">
-                    It is a long established fact that a reader will be distracted by the
-                                                    readable content.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, eos!
                 </p>
                 <p className="a-desc">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                                    aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
                 </p>
             </div>
         </div>
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,7 +2,7 @@ import './contact.css'
 import Phone from '../../img/phone.png'
 import Email from '../../img/email.png'
 import Address from '../../img/address.png'
-import {useRef} from 'react'
+import {useRef} from 'react' // accessing DOM nodes or elements directly
 
 const Contact = () => {
     const formRef = useRef();
@@ -12,48 +12,42 @@ const Contact = () => {
 
     }
     return (
-        <div className="c">
-            <div className="c-bg">
-                <div className="c-wrapper">
-                    <div className="c-left">
-                        <h1 className="c-title">Contact me</h1>
-                        <div className="c-info">
-                            <div className="c-info-item">
-                                <img src={Phone}
-                                    alt=""
-                                    className="c-icon"/>
-                                0123456789
-                            </div>
-                            <div className="c-info-item">
-                                <img src={Email}
-                                    alt=""
-                                    className="c-icon"/>
-                                info@nl.nl
-                            </div>
-                            <div className="c-info-item">
-                                <img src={Address}
-                                    alt=""
-                                    className="c-icon"/>
-                                Musterstr 1, 12345 Musterstadt
-                            </div>
-                        </div>
-                    </div>
-                    <div className="c-right">
-                        <p className="c-desc">Get in touch</p>
-                        <form ref={formRef}
-                            onSubmit={handleSubmit}>
-                            <input type="text" placeholder="Name" name="user_name"/>
-                            <input type="text" placeholder="Subject" name="user_subject"/>
-                            <input type="text" placeholder="Email" name="email"/>
-                            <textarea rows="5" placeholder="Messag" name="message"/>
-                            <button>Submit</button>
-                        </form>
-                    </div>
-                </div>
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Contact me</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +49 123456789
             </div>
-
+            <div className="c-info-item">
+              <img className="c-icon" src={Email} alt="" />
+              contact@contact.de
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="" />
+              Musterstr. 1, 12345 Musterstadt
+            </div>
+          </div>
         </div>
-    )
+        <div className="c-right">
+          <p className="c-desc">
+            <b>Get in touch. </b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate quas facilis illo quam eaque doloremque hic et quos, inventore sunt harum officiis omnis animi eos. Eveniet libero quibusdam harum?
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea rows="5" placeholder="Message" name="message" />
+            <button>Submit</button>
+           
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact
