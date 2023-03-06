97ee7bc1f47f164abcd7167adcd6587527c93963
Daniek <info@daniek.io>
2023-03-06 10:26:02 +0100

    add

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -35,9 +35,13 @@ const Contact = () => {
             });
     }
     return (
-        <div className="c">
+        <div className="c" style={{
+            backgroundColor: darkMode ? "#333" : "white"
+        }}>
             <div className="c-bg"></div>
-            <div className="c-wrapper">
+            <div className="c-wrapper" style={{
+            backgroundColor: darkMode ? "#333" : "white"
+        }}>
                 <div className="c-miniwrapper">
                     <h1 className="c-title">Contact me</h1>
 
